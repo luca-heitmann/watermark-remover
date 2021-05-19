6695d8de8e0d1269287b28de9586d674ecf8a8a6
Luca Heitmann <luca@localhost.localdomain>
2021-05-19 14:42:26 +0200

    minor changes

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-Prerequisite: Docker
+## Watermark remover
+Removes watermarks from all PDF files in the current directory and subdirectories. Requires docker and bash. Tested only lightly on Linux (Fedora 33) and Windows (WSL2 Ubuntu 20.04).
 
-Installation:
-1. Clone this repository
-2. Run ./build.sh
+Not guaranteed to produce valid PDFs.
 
-Usage: `docker run -it --rm -v "$(pwd)":/mnt/work watermark-remover "<WATERMARK_TEXT>" $(find . -type f -name "*.pdf" ! -name "*.clean.pdf")`
+### Usage
+Prerequisite:
+- Docker
+- Bash
+
+Usage:
+`docker run -it --rm -v "$(pwd)":/mnt/work lucaheitmann/watermark-remover:latest "<WATERMARK_TEXT>" $(find . -type f -name "*.pdf" ! -name "*.clean.pdf" | sed -e 's/^/"/g' -e 's/$/"/g')`
+
--- removewatermark.sh
+++ removewatermark.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 watermark="$1"
 shift 1
-files="$@"
+files=("$@")
 
-for f in $files; do
-	uncompressed_name="${f%.pdf}.uncompressed.pdf"
-	unwatermarked_name="${f%.pdf}.unwatermarked.pdf"
-	clean_name="${f%.pdf}.clean.pdf"
+for f in "${files[@]}"; do
+  echo "Processing file $f"
+  uncompressed_name="${f%.pdf}.uncompressed.pdf"
+  unwatermarked_name="${f%.pdf}.unwatermarked.pdf"
+  clean_name="${f%.pdf}.clean.pdf"
 
-	pdftk "$f" output "$uncompressed_name" uncompress
-	sed -e "s/$watermark/ /g" < "$uncompressed_name" > "$unwatermarked_name"
-	pdftk "$unwatermarked_name" output "$clean_name" compress
-	rm "$uncompressed_name" "$unwatermarked_name"
+  pdftk "$f" output "$uncompressed_name" uncompress
+  sed -e "s/$watermark/ /g" < "$uncompressed_name" > "$unwatermarked_name"
+  pdftk "$unwatermarked_name" output "$clean_name" compress
+  rm "$uncompressed_name" "$unwatermarked_name"
+  echo "Done $f"
 done
+
